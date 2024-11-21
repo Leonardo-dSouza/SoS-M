98529c7a4f73e43c979f217cc76db205d8116f17
Leonardo-dSouza <dkaireturn@gmail.com>
2024-11-21 14:02:18 -0300

    estrutura flask adicionada

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, redirect, render_template, request, session # type: ignore
+from flask_mysqldb import MySQL  # type: ignore
+import io
+
+app = Flask(__name__)
+
+
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'senha'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_DB'] = 'SoS-M'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug= True)
---
+++ docs/comandos.md
@@ -0,0 +1,18 @@
+cd {nome da pasta} = acessa a página
+
+pwd == para ver a pasta
+
+python -m venv venv = cria um ambiente virtual
+
+.\{nome do venv}\Scripts\activate = ativa o ambiente
+deactivate = desativa
+
+pip install flask = instala o flask
+
+pip freeze = mostra as dependencias
+
+pip freeze > requirements.txt = cria uma saida com os requisitos em texto
+
+pip install -r .\requirements.txt = para instalar os requirements
+
+code . = abre o vscode na pasta selecionada
